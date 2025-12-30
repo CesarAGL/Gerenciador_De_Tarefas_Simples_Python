31962f190991f7da543cf780b1149c5fda1786da
CesarAGL <xcesaraugustoz@gmail.com>
2025-12-30 14:25:34 -0300

    Update __init__.py

--- GerenciadordeTarefas/biblioteca/interface/__init__.py
+++ GerenciadordeTarefas/biblioteca/interface/__init__.py
@@ -1,4 +1,10 @@
 def leiainteiro(msg):
+    """
+    Lê um número inteiro do usuário e trata erros, como erros de digitação e interrupção.
+
+    param msg: mensagem exibida para o usuário
+    return: número inteiro digitado ou 0 em caso de interrupção da entrada
+    """
     while True:
         try:
             n = int(input(msg))
@@ -11,15 +17,33 @@ def leiainteiro(msg):
         else:
             return n
 def linha(tamanho = 46):
+    """
+    Retorna uma linha decorativa para a formatação do menu
+
+    param tamanho: tamanho da linha (quantidade de caracteres)
+    return: string formatada
+    """
     return ('-=' * 46)
 
 def header(txt):
+    """
+    Exibe um cabeçalho formatado com linhas decorativas
+
+    param txt: texto que será exibido no cabeçalho
+    """
     print(linha())
     print(txt.center(46))
     print(linha())
 
 
 def menu(vetor):
+    """
+    exibe um menu numerado a partir de uma lista de opções
+    e retorna a opção escolhida pelo usuário
+
+    param vetor: lista de opções do menu
+    return número que corresponde a opção escolhida
+    """
     header('MENU PRINCIPAL')
     c = 1
     for item in vetor:
